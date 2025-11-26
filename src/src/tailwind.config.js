01b0f93999a2474bfe2a0e17b012302bba4905d9
rakeshsp <rakesh.jnv21@gmail.com>
2025-11-27 00:33:23 +0530

    Update tailwind.config.js

--- src/src/tailwind.config.js
+++ src/src/tailwind.config.js
@@ -1,1 +1,12 @@
 
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
